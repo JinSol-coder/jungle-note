1562d5b102d3397922c87df6ece6ad8b984537d5
unknown <imwe25@naver.com>
2025-07-08 08:53:38 +0900

    기초세팅

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, redirect, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+   return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
+@app.route('/register')
+def register():
+   return render_template('register.html')
+
+@app.route('/main')
+def main():
+   return 'This is main page!'
+
+@app.route('/repeat')
+def repeat():
+   return 'This is repeat page!'
+
+if __name__ == '__main__':  
+   app.run('0.0.0.0', port=5000, debug=True)
