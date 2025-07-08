c051ccd27db6918e6a8b420ae095162d4e901582
unknown <imwe25@naver.com>
2025-07-08 19:54:00 +0900

    메모 추가 페이지 ui 및 메모 기능 부여

--- app.py
+++ app.py
@@ -21,5 +21,9 @@ def main():
 def repeat():
    return 'This is repeat page!'
 
+@app.route('/memo_add')
+def memo_add():
+   return render_template('memo_add.html')
+
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
